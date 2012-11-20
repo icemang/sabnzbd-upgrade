cae016323407751592b36c0b8361c34499963f5c
Charlie Root <icemang@gmail.com>
2012-11-19 22:09:24 -0700

    working copy

--- sabnzbd_upgrade.sh
+++ sabnzbd_upgrade.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/local/bin/bash
 ############################################################################################################
 #
 # -- USER ADJUSTABLE VARIABLES --
@@ -6,50 +6,69 @@
 #  SABDIR needs your local SABnzbd directory location
 #  SABCONFIGDIR needs your local SABnzbd CONFIG directory location (Sometimes this is the same as SABDIR)
 
-HOST="localhost"
+HOST="10.0.0.1"
 SABDIR="SABnzbd"
 SABDESTDIR="/datapool/systemfiles"
 SABCONFIGDIR="/datapool/systemfiles/SABnzbd.Config"
-TEMPDIR="~/sabupgradetemp907821034"
 
-#For Archive - enter yes or no (if yes, specify a direct path)
-ARCHIVE="no"
+#For Archive - enter 0 for no or 1 for yes (if yes, specify a direct path)
+ARCHIVE="0"
 ARCHIVEPATH=""
+
+#If you are on the current version but want to force an update, change the below variable to 1
+# Note that changing the below variable to 1 WILL force a reinstall everytime the script runs.
+START_UPDATE="0"
 
 #
 # -- DONE WITH USER INPUT --
 ############################################################################################################
 
-API_KEY=`cat ${SABCONFIGDIR}/sabnzbd.ini | grep ^api_key | awk '{print $3}'`
-PORT=`cat ${SABCONFIGDIR}/sabnzbd.ini | grep ^https_port | awk '{print $3}'`
+LOCAL_VERSION=`cat ${SABDESTDIR}/${SABDIR}/PKG-INFO | grep ^Version | awk '{print $2}'`
 VERSION=`curl -s http://sabnzbdplus.sourceforge.net/version/latest | head -n1`
 
-#added this to work on checking local version
-LOCAL_VERSION=`cat ${SABDIR}/PKG-INFO | grep ^version | awk '{print $3}'`
+if [ "${VERSION}" != "${LOCAL_VERSION}" ]; then
+START_UPDATE="1"
+fi
 
-DIR="SABnzbd-${VERSION}"
-GZ="${DIR}-src.tar.gz"
-DATE=`date +'%Y%m%d-%H%M'`
+if [ "${START_UPDATE}" = "0" ]; then
+        echo "You are on the current version of: ${LOCAL_VERSION}"
+        echo "No update will be performed"
+else
+	API_KEY=`cat ${SABCONFIGDIR}/sabnzbd.ini | grep ^api_key | awk '{print $3}'`
+	PORT=`cat ${SABCONFIGDIR}/sabnzbd.ini | grep ^port | awk '{print $3}' | head -1`
+	DIR="SABnzbd-${VERSION}"
+	GZ="${DIR}-src.tar.gz"
 
-cd ${SABDESTDIR}
+        cd ${SABDESTDIR}
+        echo "New version found! You are on ${LOCAL_VERSION}, upgrading to ${VERSION} now."
 
-echo "Downloading SABnzbd ${VERSION} (${GZ})"
-curl -s -C - -O "http://freefr.dl.sourceforge.net/project/sabnzbdplus/sabnzbdplus/${VERSION}/${GZ}" | tar -xzf -
+	echo "Downloading SABnzbd ${VERSION} (${GZ})"
+	curl -s -C - -O "http://freefr.dl.sourceforge.net/project/sabnzbdplus/sabnzbdplus/${VERSION}/${GZ}"
 
-# Alternative source: SWITCH (ch)
-# curl -s -C - -O "http://switch.dl.sourceforge.net/project/sabnzbdplus/sabnzbdplus/0.7.3/SABnzbd-0.7.3-src.tar.gz" | tar -xzf -
+	# Alternative source: SWITCH (ch)
+	# curl -s -C - -O "http://switch.dl.sourceforge.net/project/sabnzbdplus/sabnzbdplus/${VERSION}/${GZ}"
 
-echo "Shutting down SABnzbd+"
-curl -s "http://${HOST}:${PORT}/sabnzbd/api?mode=shutdown&apikey=${API_KEY}" >> /dev/null
+	echo "Unpacking ${GZ}"
+	tar -xzf ${GZ}
+	rm ${GZ}
 
-echo "Installing new SABnzbd+"
-mkdir -p ~/archives/
-mv ${SABDIR} ~/archives/SABnzbd_${DATE}
-mv ${DIR} ${SABDIR}
+	echo "Shutting down SABnzbd+"
+	curl -s "http://${HOST}:${PORT}/sabnzbd/api?mode=shutdown&apikey=${API_KEY}" >> /dev/null
 
-echo "Restarting SABnzdb+"
-python ${SABDIR}/SABnzbd.py -d -f ${SABCONFIGDIR}/sabnzbd.ini > /dev/null
+	echo "Installing new SABnzbd+"
+	if [ "${ARCHIVE}" = "0" ]; then
+		rm -rf ${SABDIR}
+	else
+#		DATE=`date +'%Y%m%d-%H%M'`
+#		mkdir -p ${ARCHIVEPATH}
+#		mv ${SABDIR} ${ARCHIVEPATH}_${DATE}
+	fi
+	mv ${DIR} ${SABDIR}
 
-# Go back to the previous directory 
-cd -
-echo "Upgrade complete !"
+	echo "Restarting SABnzdb+"
+	python ${SABDIR}/SABnzbd.py -d -f ${SABCONFIGDIR}/sabnzbd.ini >> /dev/null
+
+	# Go back to the previous directory
+	cd -
+	echo "Upgrade complete !"
+fi
